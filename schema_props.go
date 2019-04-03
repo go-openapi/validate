7834a5890e03de2a11fecb59817ea5666e705dad
Xiaoxi He <xxhe@alauda.io>
2019-04-03 15:43:38 +0800

    Fix schema options aren't propagated to childs
    Signed-off-by: Xiaoxi He <xxhe@alauda.io>

--- schema_props.go
+++ schema_props.go
@@ -36,31 +36,36 @@ type schemaPropsValidator struct {
 	notValidator    *SchemaValidator
 	Root            interface{}
 	KnownFormats    strfmt.Registry
+	Options         SchemaValidatorOptions
 }
 
 func (s *schemaPropsValidator) SetPath(path string) {
 	s.Path = path
 }
 
-func newSchemaPropsValidator(path string, in string, allOf, oneOf, anyOf []spec.Schema, not *spec.Schema, deps spec.Dependencies, root interface{}, formats strfmt.Registry) *schemaPropsValidator {
+func newSchemaPropsValidator(path string, in string, allOf, oneOf, anyOf []spec.Schema, not *spec.Schema, deps spec.Dependencies, root interface{}, formats strfmt.Registry, options ...Option) *schemaPropsValidator {
 	anyValidators := make([]SchemaValidator, 0, len(anyOf))
 	for _, v := range anyOf {
-		anyValidators = append(anyValidators, *NewSchemaValidator(&v, root, path, formats))
+		anyValidators = append(anyValidators, *NewSchemaValidator(&v, root, path, formats, options...))
 	}
 	allValidators := make([]SchemaValidator, 0, len(allOf))
 	for _, v := range allOf {
-		allValidators = append(allValidators, *NewSchemaValidator(&v, root, path, formats))
+		allValidators = append(allValidators, *NewSchemaValidator(&v, root, path, formats, options...))
 	}
 	oneValidators := make([]SchemaValidator, 0, len(oneOf))
 	for _, v := range oneOf {
-		oneValidators = append(oneValidators, *NewSchemaValidator(&v, root, path, formats))
+		oneValidators = append(oneValidators, *NewSchemaValidator(&v, root, path, formats, options...))
 	}
 
 	var notValidator *SchemaValidator
 	if not != nil {
-		notValidator = NewSchemaValidator(not, root, path, formats)
+		notValidator = NewSchemaValidator(not, root, path, formats, options...)
 	}
 
+	schOptions := &SchemaValidatorOptions{}
+	for _, o := range options {
+		o(schOptions)
+	}
 	return &schemaPropsValidator{
 		Path:            path,
 		In:              in,
@@ -75,6 +80,7 @@ func newSchemaPropsValidator(path string, in string, allOf, oneOf, anyOf []spec.
 		notValidator:    notValidator,
 		Root:            root,
 		KnownFormats:    formats,
+		Options:         *schOptions,
 	}
 }
 
@@ -209,7 +215,7 @@ func (s *schemaPropsValidator) Validate(data interface{}) *Result {
 			if dep, ok := s.Dependencies[key]; ok {
 
 				if dep.Schema != nil {
-					mainResult.Merge(NewSchemaValidator(dep.Schema, s.Root, s.Path+"."+key, s.KnownFormats).Validate(data))
+					mainResult.Merge(NewSchemaValidator(dep.Schema, s.Root, s.Path+"."+key, s.KnownFormats, s.Options.Options()...).Validate(data))
 					continue
 				}
 
