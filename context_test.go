42624cc6cadce122cea8f6357f374c9fd34cf041
Youyuan Wu <youyuanwu@outlook.com>
2020-09-08 22:22:13 -0700

    add readonly validate
    Signed-off-by: Youyuan Wu <youyuanwu@outlook.com>

---
+++ context_test.go
@@ -0,0 +1,50 @@
+package validate
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestContext_ExtractOperationType(t *testing.T) {
+
+	var testCases = []struct {
+		Ctx            context.Context
+		ExpectedOpType operationType
+	}{
+		{
+			Ctx:            WithOperationRequest(context.Background()),
+			ExpectedOpType: request,
+		},
+		{
+			Ctx:            WithOperationResponse(context.Background()),
+			ExpectedOpType: response,
+		},
+		{
+			Ctx:            context.Background(),
+			ExpectedOpType: none,
+		},
+		{
+			Ctx:            context.WithValue(context.Background(), validateCtxKey("dummy"), "dummy val"),
+			ExpectedOpType: none,
+		},
+		{
+			Ctx:            context.WithValue(context.Background(), operationTypeKey, "dummy val"),
+			ExpectedOpType: none,
+		},
+		{
+			Ctx:            context.WithValue(context.Background(), operationTypeKey, operationType("dummy val")),
+			ExpectedOpType: none,
+		},
+	}
+
+	for idx, tc := range testCases {
+		t.Run(fmt.Sprintf("TestCase #%d", idx), func(t *testing.T) {
+			op := extractOperationType(tc.Ctx)
+			assert.Equal(t, tc.ExpectedOpType, op)
+		})
+	}
+
+}
